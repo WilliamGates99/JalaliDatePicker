e04da78c1a9736db402976d2865730638e21f57a
William <william.gates.3299@gmail.com>
2025-01-31 18:17:26 +0330

    Initial Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,108 @@
+@file:Suppress("UnstableApiUsage")
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "com.xeniac.datepickerdemo"
+    compileSdk = 35
+    buildToolsVersion = "35.0.0"
+
+    defaultConfig {
+        applicationId = "com.xeniac.datepickerdemo"
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    androidResources {
+        generateLocaleConfig = true
+    }
+
+    buildTypes {
+        debug {
+            versionNameSuffix = " - Debug"
+            applicationIdSuffix = ".dev.debug"
+        }
+
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            ndk.debugSymbolLevel = "FULL" // Include native debug symbols file in app bundle
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        buildConfig = true
+        compose = true
+    }
+
+    compileOptions {
+        // Java 8+ API Desugaring Support
+        isCoreLibraryDesugaringEnabled = true
+
+        sourceCompatibility = JavaVersion.VERSION_22
+        targetCompatibility = JavaVersion.VERSION_22
+    }
+
+    kotlinOptions {
+        jvmTarget = "22"
+    }
+
+    bundle {
+        language {
+            /*
+            Specifies that the app bundle should not support configuration APKs for language resources.
+            These resources are instead packaged with each base and dynamic feature APK.
+             */
+            enableSplit = false
+        }
+    }
+}
+
+dependencies {
+    // Java 8+ API Desugaring Support
+    coreLibraryDesugaring(libs.desugar.jdk.libs)
+
+    implementation(libs.bundles.essentials)
+
+    // Jetpack Compose
+    implementation(platform(libs.compose.bom))
+    implementation(libs.bundles.compose)
+
+    // Architectural Components
+    implementation(libs.bundles.architectural.components)
+
+    // Coroutines
+    implementation(libs.bundles.coroutines)
+
+    // Timber Library
+    implementation(libs.timber)  // Local Unit Test Libraries
+    testImplementation(libs.bundles.local.unit.tests)
+
+    // Instrumentation Test Libraries
+    androidTestImplementation(libs.bundles.instrumentation.tests)
+
+    // UI Test Libraries
+    androidTestImplementation(libs.bundles.ui.tests)
+    androidTestImplementation(platform(libs.compose.bom))
+    debugImplementation(libs.compose.ui.test.manifest)
+}
---
+++ app/src/main/java/com/xeniac/datepickerdemo/ui/theme/Type.kt
@@ -0,0 +1,5 @@
+package com.xeniac.datepickerdemo.ui.theme
+
+import androidx.compose.material3.Typography
+
+val Typography = Typography()
