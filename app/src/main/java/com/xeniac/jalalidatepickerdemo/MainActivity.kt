337748b7fc82892524d51665f958829e45d7c919
William <william.gates.3299@gmail.com>
2025-02-01 23:31:43 +0330

    Implemented date picker UI

--- app/src/main/java/com/xeniac/jalalidatepickerdemo/MainActivity.kt
+++ app/src/main/java/com/xeniac/jalalidatepickerdemo/MainActivity.kt
@@ -4,11 +4,27 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.xeniac.jalalidatepicker.JalaliDatePicker
+import com.xeniac.jalalidatepicker.models.SelectedJalaliDate
 import com.xeniac.jalalidatepickerdemo.ui.theme.JalaliDatePickerTheme
 
 class MainActivity : AppCompatActivity() {
@@ -21,11 +37,64 @@ class MainActivity : AppCompatActivity() {
                 Scaffold(
                     modifier = Modifier.fillMaxSize()
                 ) { innerPadding ->
-                    JalaliDatePicker(
-                        modifier = Modifier.padding(innerPadding)
+                    JalaliDatePickerDemo(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
                     )
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun JalaliDatePickerDemo(
+    modifier: Modifier = Modifier,
+    contentPaddingValues: PaddingValues = PaddingValues(horizontal = 24.dp)
+) {
+    var selectedJalaliDate by remember {
+        mutableStateOf(
+            SelectedJalaliDate(
+                year = 0,
+                month = 0,
+                day = 0
+            )
+        )
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(
+            space = 36.dp,
+            alignment = Alignment.CenterVertically
+        ),
+        modifier = modifier.padding(contentPaddingValues)
+    ) {
+        JalaliDatePicker(
+            onSelectedDateChange = {
+                selectedJalaliDate = it
+            },
+            isSelectFromFutureEnabled = true,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Text(
+            text = with(selectedJalaliDate) {
+                "$year/$month/$day"
+            },
+            style = MaterialTheme.typography.displayMedium,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize()
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun JalaliDatePickerDemoPreview() {
+    JalaliDatePickerTheme {
+        JalaliDatePickerDemo()
     }
 }
