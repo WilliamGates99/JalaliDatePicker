36619c26e997bd2b83af605eecd28a77772722c9
William <william.gates.3299@gmail.com>
2025-03-10 23:26:46 +0330

    Improved sample app theme

--- sample/src/main/java/com/xeniac/jalalidatepickerdemo/ui/theme/Theme.kt
+++ sample/src/main/java/com/xeniac/jalalidatepickerdemo/ui/theme/Theme.kt
@@ -11,21 +11,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Blue
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Blue
 )
 
 @Composable
 fun JalaliDatePickerTheme(
     isDarkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = true, // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = false, // Dynamic color is available on Android 12+
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
